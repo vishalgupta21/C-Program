cad4369b0c1876ed84237b3a5b5c6ac4f0263428
vishalgupta21 <55844688+vishalgupta21@users.noreply.github.com>
2020-05-18 00:02:12 +0530

    Create Print_Poem.c

---
+++ Print_Poem.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+	printf("Mary had a little lamb,\n");
+	printf("Little lamb, little lamb,\n");
+	printf("Mary had a little lamb,\n");
+	printf("Its fleece was white as snow,\n");
+	printf("And every where that Mary went,\n");
+	printf("Mary went, Mary went\n");
+	printf("Everywhere that Mary went,\n");
+
+   return 0;
+
+}    
